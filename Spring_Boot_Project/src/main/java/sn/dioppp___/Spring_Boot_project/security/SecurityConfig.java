5b36ed9951b427fbb92a6ed869ea7dc14aa98b77
DiopAmary <amarydiop27@gmail.com>
2021-04-12 13:10:51 +0000

    Mise en palce d'un système d’authentification basé sur une configuration JDBC Authentication

--- Spring_Boot_Project/src/main/java/sn/dioppp___/Spring_Boot_project/security/SecurityConfig.java
+++ Spring_Boot_Project/src/main/java/sn/dioppp___/Spring_Boot_project/security/SecurityConfig.java
@@ -1,5 +1,8 @@
 package sn.dioppp___.Spring_Boot_project.security;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -10,11 +13,26 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private DataSource dataSource;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		PasswordEncoder passwordEncoder = passwordEncoder();
+		/*System.out.println("*************************************");
+		System.out.println(passwordEncoder.encode("123456"));
+		System.out.println("*************************************");
 		auth.inMemoryAuthentication().withUser("dioppp___").password(passwordEncoder.encode("123456")).roles("USER", "ADMIN");
-		auth.inMemoryAuthentication().withUser("amary").password(passwordEncoder.encode("123456")).roles("USER");
+		auth.inMemoryAuthentication().withUser("amary").password(passwordEncoder.encode("123456")).roles("USER");*/
+		
+		
+		//système d’authentification basé sur une configuration JDBC Authentication
+		auth.jdbcAuthentication()
+			.dataSource(dataSource)
+			.usersByUsernameQuery("select username as principal, password as credential, active from users where username=?")
+			.authoritiesByUsernameQuery("select username as pricipal, roles from users_roles where username=?")
+			.passwordEncoder(passwordEncoder);
+			
 	}
 	
 	@Override
