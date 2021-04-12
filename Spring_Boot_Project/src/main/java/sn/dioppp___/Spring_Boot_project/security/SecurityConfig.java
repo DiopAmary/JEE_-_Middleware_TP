111bc6f6ad8c9b17cdc19d7dba74099792efb8b5
DiopAmary <amarydiop27@gmail.com>
2021-04-12 12:06:40 +0000

    Mise en place un système d’authentification basé sur une configuration In Memory Authentication

---
+++ Spring_Boot_Project/src/main/java/sn/dioppp___/Spring_Boot_project/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package sn.dioppp___.Spring_Boot_project.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("dioppp___").password("{noop}123456").roles("USER", "ADMIN");
+		auth.inMemoryAuthentication().withUser("amary").password("{noop}123456").roles("USER");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.formLogin();
+		http.authorizeRequests().antMatchers("/edit**", "/ajouter**", "/delete**").hasRole("ADMIN");
+		http.authorizeRequests().anyRequest().authenticated();
+	}
+
+}
